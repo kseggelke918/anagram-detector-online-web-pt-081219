be296ec0eb6d98af2f90006afca98cc7ddb7955b
Katie Seggelke <katie.seggelke@gmail.com>
2019-09-09 15:16:24 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word 
+  
+  
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(anagram_array) 
+    #return all matches in an array of possible anagrams in an array and return an empty array if none
+    
+  end 
+  
+  
+  
+  
+  
+  
+  
+  
+end 
